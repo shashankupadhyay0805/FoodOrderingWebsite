0ea0a958df6632e18adfb0b1f4b5d07b21c57b3c
shashankupadhyay0805 <147583703+shashankupadhyay0805@users.noreply.github.com>
2025-11-09 01:29:42 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import AddRestaurant from "./components/admin/AddRestaurant";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import AddMenu from "./components/admin/AddMenu";
-// import ManageOrder from "./components/admin/ManageOrder";
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/admin/add-restaurant" element={<AddRestaurant />} />
-//         <Route path="/admin/add-menu/:restaurantId" element={<AddMenu />} />
-//         <Route path="/admin/manage-orders" element={<ManageOrder />} />
-//       </Routes>
-//       <ToastContainer />
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
-
-
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AddRestaurant from "./components/admin/AddRestaurant";
@@ -31,23 +5,49 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AddMenu from "./components/admin/AddMenu";
 import ManageOrder from "./components/admin/ManageOrder";
-// 1. Import the new component
-import RestaurantList from "./components/admin/RestaurantList";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {/* 2. Add the new route to your list */}
-        <Route path="/admin/restaurants" element={<RestaurantList />} />
-        <Route path="/admin/add-restaurant" element={<AddRestaurant />} />
-        <Route path="/admin/add-menu/:restaurantId" element={<AddMenu />} />
-        <Route path="/admin/manage-orders" element={<ManageOrder />} />
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/admin/add-restaurant" element={<AddRestaurant />} />
+        <Route path="/admin/add-menu/:restaurantId" element={<AddMenu />} />
+        <Route path="/admin/manage-orders" element={<ManageOrder />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
+
+
+
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import AddRestaurant from "./components/admin/AddRestaurant";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import AddMenu from "./components/admin/AddMenu";
+// import ManageOrder from "./components/admin/ManageOrder";
+// // 1. Import the new component
+// import RestaurantList from "./components/admin/RestaurantList";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         {/* 2. Add the new route to your list */}
+//         <Route path="/admin/restaurants" element={<RestaurantList />} />
+//         <Route path="/admin/add-restaurant" element={<AddRestaurant />} />
+//         <Route path="/admin/add-menu/:restaurantId" element={<AddMenu />} />
+//         <Route path="/admin/manage-orders" element={<ManageOrder />} />
+//       </Routes>
+//       <ToastContainer />
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
