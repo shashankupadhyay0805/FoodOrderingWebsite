66db20e3f773a31cfe91ec8c74233c88da608b77
shashankupadhyay0805 <147583703+shashankupadhyay0805@users.noreply.github.com>
2025-11-09 16:25:17 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -27,6 +27,69 @@
 
 // export default App;
 
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// // --- START: IMPORTS ---
+// import AddRestaurant from "./components/admin/AddRestaurant";
+// import AddMenu from "./components/admin/AddMenu";
+// import ManageOrder from "./components/admin/ManageOrder";
+// import RestaurantList from "./components/admin/RestaurantList";
+
+// import AdminLogin from "./components/adminpage/AdminLogin.jsx";
+// import AdminProtectedRoute from "./components/adminpage/AdminProtectedRoute.jsx";
+// import ManageMenus from "./components/adminpage/ManageMenus.jsx"; 
+
+// // 1. Import your new ManageUsers component
+// // (Make sure this file exists at this path)
+// import ManageUsers from "./components/adminpage/ManageUsers.jsx"; 
+
+// // Import your layout
+// import AdminLayout from "./components/AdminLayout.jsx"; 
+// // --- END: IMPORTS ---
+
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         {/* ROUTE 1: THE LOGIN PAGE */}
+//         <Route path="/admin-login" element={<AdminLogin />} />
+
+//         {/* ROUTE 2: YOUR PROTECTED ADMIN PAGES */}
+//         <Route
+//           path="/admin"
+//           element={
+//             <AdminProtectedRoute>
+//               <AdminLayout />
+//             </AdminProtectedRoute>
+//           }
+//         >
+//           {/* Child routes rendered inside AdminLayout */}
+//           <Route index element={<RestaurantList />} />
+//           <Route path="restaurants" element={<RestaurantList />} />
+//           <Route path="add-restaurant" element={<AddRestaurant />} />
+//           <Route path="add-menu/:restaurantId" element={<AddMenu />} />
+//           <Route path="manage-orders" element={<ManageOrder />} />
+// M       <Route path="menus" element={<ManageMenus />} />
+          
+//           {/* 2. Add the new route for Users */}
+//           <Route path="users" element={<ManageUsers />} />
+
+//         </Route>
+
+//         {/* A default route to send users to login */}
+//         <Route path="/" element={<AdminLogin />} />
+//       </Routes>
+//       <ToastContainer />
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -35,17 +98,12 @@ import "react-toastify/dist/ReactToastify.css";
 import AddRestaurant from "./components/admin/AddRestaurant";
 import AddMenu from "./components/admin/AddMenu";
 import ManageOrder from "./components/admin/ManageOrder";
-import RestaurantList from "./components/admin/RestaurantList";
+import RestaurantList from "./components/admin/RestaurantList"; // This is the component we want to show
 
 import AdminLogin from "./components/adminpage/AdminLogin.jsx";
 import AdminProtectedRoute from "./components/adminpage/AdminProtectedRoute.jsx";
-import ManageMenus from "./components/adminpage/ManageMenus.jsx"; 
-
-// 1. Import your new ManageUsers component
-// (Make sure this file exists at this path)
+// import ManageMenus from "./components/adminpage/ManageMenus.jsx"; // We don't need this file anymore
 import ManageUsers from "./components/adminpage/ManageUsers.jsx"; 
-
-// Import your layout
 import AdminLayout from "./components/AdminLayout.jsx"; 
 // --- END: IMPORTS ---
 
@@ -54,10 +112,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* ROUTE 1: THE LOGIN PAGE */}
         <Route path="/admin-login" element={<AdminLogin />} />
 
-        {/* ROUTE 2: YOUR PROTECTED ADMIN PAGES */}
         <Route
           path="/admin"
           element={
@@ -66,20 +122,19 @@ const App = () => {
             </AdminProtectedRoute>
           }
         >
-          {/* Child routes rendered inside AdminLayout */}
           <Route index element={<RestaurantList />} />
           <Route path="restaurants" element={<RestaurantList />} />
           <Route path="add-restaurant" element={<AddRestaurant />} />
           <Route path="add-menu/:restaurantId" element={<AddMenu />} />
           <Route path="manage-orders" element={<ManageOrder />} />
-M       <Route path="menus" element={<ManageMenus />} />
           
-          {/* 2. Add the new route for Users */}
+          {/* --- THIS IS THE FIX --- */}
+          {/* We've changed the element from ManageMenus to RestaurantList */}
+          <Route path="menus" element={<RestaurantList />} />
+          
           <Route path="users" element={<ManageUsers />} />
-
         </Route>
 
-        {/* A default route to send users to login */}
         <Route path="/" element={<AdminLogin />} />
       </Routes>
       <ToastContainer />
@@ -88,9 +143,6 @@ M       <Route path="menus" element={<ManageMenus />} />
 };
 
 export default App;
-
-
-
 
 
 
